b3ec513e2da90e65a0eae2eb4f7f5fa3401b6018
gheor <gheor@SurfaceProi7-GAP>
2019-08-12 14:25:44 +0300

    First commit to a-sti.ro repository - Java Training

---
+++ JavaTraining/src/javatraining/JavaTraining.java
@@ -0,0 +1,19 @@
+/*
+ * Teste repository new created.
+ */
+package javatraining;
+
+/**
+ * test new created repository.
+ * @author gheor
+ */
+public class JavaTraining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a new message!");
+    }
+    
+}
